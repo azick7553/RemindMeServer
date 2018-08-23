19fc18ad60f18be969275d713c7c946f2a767aed
Azick <Azick@AZICK-PC>
2018-08-23 17:32:54 +0500

    finish course, created CRUD

--- src/main/java/com/devmore/remindme/server/controller/ReminderController.java
+++ src/main/java/com/devmore/remindme/server/controller/ReminderController.java
@@ -5,6 +5,8 @@ import com.devmore.remindme.server.service.ReminderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -17,6 +19,22 @@ public class ReminderController {
     @ResponseBody
     public List<Remind> getAllReminders(){
         return service.getAll();
+    }
+
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    @ResponseBody
+    public List<Remind> getRemind(){
+        List<Remind> list = new ArrayList<Remind>();
+        Remind r = new Remind();
+        r.setId(1);
+        r.setTitle("test");
+        r.setRemindDate(new Date());
+        list.add(r);
+        r.setId(1);
+        r.setTitle("test2");
+        r.setRemindDate(new Date());
+        list.add(r);
+        return list;
     }
 
     @RequestMapping(value = "/reminders/{id}", method = RequestMethod.GET)
